bdf24ed8b3074a1c0b397c7397776e860afae771
amyard <maksymturenko@gmail.com>
2020-09-02 01:18:47 +0300

    3.7. add register validation errors. update postman collection

--- Backend/Dtos/RegisterDto.cs
+++ Backend/Dtos/RegisterDto.cs
@@ -1,9 +1,19 @@
-﻿namespace Backend.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Backend.Dtos
 {
     public class RegisterDto
     {
+        [Required]
         public string DisplayName { get; set; }
+
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+
+        [Required]
+        [RegularExpression("(?=^.{6,10}$)(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\\s).*$", 
+        ErrorMessage="Password must have 1 Uppercase, 1 Lowercase, 1 number, 1 non alphameric and least 6 characters")]
         public string Password { get; set; }
     }
 }
